b67ce5b54249726aba702bf6ff43dbf5d827c0f4
dnitra <109307365+dnitra@users.noreply.github.com>
2023-04-25 20:45:35 +0200

    prepared tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    //test this without vite
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->withoutVite();
+    }
 }
